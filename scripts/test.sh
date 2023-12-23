bae3431cb153b6638c429769de9d3f76296b3ec0
Zhentao Huang <zhentaohuang222@gmail.com>
2023-12-22 19:44:34 -0500

    Create test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,18 @@
+python3 main_SealNeRF.py data/nerf_synthetic/lego/\
+    --workspace exps/lego_ngp_bbox -O --bound 1.0 --scale 0.8 --dt_gamma 0\
+    --pretraining_epochs 100 --extra_epochs 150\
+    --pretraining_local_point_step 0.005 --pretraining_surrounding_point_step -1\
+    --pretraining_lr 0.05 --ckpt exps/lego_ngp_new/checkpoints/ngp_ep0300.pth\
+    --teacher_workspace exps/lego_ngp --teacher_ckpt exps/lego_ngp_new/checkpoints/ngp_ep0300.pth\
+    --seal_config data/seal/lego_bbox/\
+    --eval_interval 100 --eval_count 10 --gui
+
+
+python main_seald.py data/dnerf/lego --workspace dnerf-jumpingjackslayer5-1103-2-t128 -O --bound 1.0 --scale 0.8 --dt_gamma 0 --lr 5e-4 --lr_net 5e-5 --gui --teacher_workspace trial_seald_teacher
+
+
+python main_dnerf.py data/dnerf/standup --workspace exps/standup/layer8-t64-1206 --bound 1.0 --scale 0.8 --dt_gamma 0  --lr 5e-4 --lr_net 5e-4 -O --iters 300000
+
+
+# wrong results
+python main_dnerf.py data/dnerf/lego --workspace dnerf-legolayer16-400-1031-new --bound 1.0 --scale 0.8 --dt_gamma 0  --lr 5e-4 --lr_net 5e-4 -O --iters 800000 
